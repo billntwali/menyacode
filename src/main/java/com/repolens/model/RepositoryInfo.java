9952cf271d933f5ef28b6bd8f725549ff6659e95
Bill Ntwali <billntwali996@gmail.com>
2026-07-15 20:47:14 -0400

    Expand repository metadata model

--- src/main/java/com/repolens/model/RepositoryInfo.java
+++ src/main/java/com/repolens/model/RepositoryInfo.java
@@ -4,5 +4,12 @@ public record RepositoryInfo(
         String owner,
         String name,
         String defaultBranch,
-        String htmlUrl
+        String htmlUrl,
+        String description,
+        String language,
+        int stars,
+        int forks,
+        String license,
+        String updatedAt,
+        java.util.List<String> topics
 ) {}
